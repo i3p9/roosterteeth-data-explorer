718c58e965b76ae2a906db7d500e58a012d44b45
i3p9 <i3p9@users.noreply.github.com>
2024-03-08 16:30:02 +0600

    init: v0 of roosterteeth data explorer

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container mx-auto px-4 py-2">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/data/utils/data.js
@@ -0,0 +1,194 @@
+export const channels =
+    [{
+        links: {
+            episodes: "/api/v1/channels/achievement-hunter/episodes",
+            featured_items: "/api/v1/channels/achievement-hunter/featured_items",
+            livestreams: "/api/v1/channels/achievement-hunter/livestreams",
+            product_collections: "/api/v1/channels/achievement-hunter/products",
+            self: "/api/v1/channels/achievement-hunter",
+            shows: "/api/v1/channels/achievement-hunter/shows"
+        },
+        name: "Achievement Hunter",
+        slug: "achievement-hunter",
+        uuid: "2cb2a70c-be50-46f5-93d7-84a1baabb4f7"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/all-good-no-worries/episodes",
+            featured_items: "/api/v1/channels/all-good-no-worries/featured_items",
+            livestreams: "/api/v1/channels/all-good-no-worries/livestreams",
+            product_collections: "/api/v1/channels/all-good-no-worries/products",
+            self: "/api/v1/channels/all-good-no-worries",
+            shows: "/api/v1/channels/all-good-no-worries/shows"
+        },
+        name: "All Good No Worries",
+        slug: "all-good-no-worries",
+        uuid: "b0252a94-e97d-4784-9fc0-03b97be9df38"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/best-friends-today/episodes",
+            featured_items: "/api/v1/channels/best-friends-today/featured_items",
+            livestreams: "/api/v1/channels/best-friends-today/livestreams",
+            product_collections: "/api/v1/channels/best-friends-today/products",
+            self: "/api/v1/channels/best-friends-today",
+            shows: "/api/v1/channels/best-friends-today/shows"
+        },
+        name: "Best Friends Today",
+        slug: "best-friends-today",
+        uuid: "d679726b-c41e-4044-85b3-3a088739c0e6"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/camp-camp/episodes",
+            featured_items: "/api/v1/channels/camp-camp/featured_items",
+            livestreams: "/api/v1/channels/camp-camp/livestreams",
+            product_collections: "/api/v1/channels/camp-camp/products",
+            self: "/api/v1/channels/camp-camp",
+            shows: "/api/v1/channels/camp-camp/shows"
+        },
+        name: "Camp Camp",
+        slug: "camp-camp",
+        uuid: "a0b578e2-56d7-4b60-8432-b97ba3426aaa"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/death-battle/episodes",
+            featured_items: "/api/v1/channels/death-battle/featured_items",
+            livestreams: "/api/v1/channels/death-battle/livestreams",
+            product_collections: "/api/v1/channels/death-battle/products",
+            self: "/api/v1/channels/death-battle",
+            shows: "/api/v1/channels/death-battle/shows"
+        },
+        name: "Death Battle",
+        slug: "death-battle",
+        uuid: "dd838359-a0e0-405f-b18b-5b0ed16ef852"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/dogbark/episodes",
+            featured_items: "/api/v1/channels/dogbark/featured_items",
+            livestreams: "/api/v1/channels/dogbark/livestreams",
+            product_collections: "/api/v1/channels/dogbark/products",
+            self: "/api/v1/channels/dogbark",
+            shows: "/api/v1/channels/dogbark/shows"
+        },
+        name: "Dogbark",
+        slug: "dogbark",
+        uuid: "ef34750e-ab1c-4c29-985f-cf42b066102b"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/f-kface/episodes",
+            featured_items: "/api/v1/channels/f-kface/featured_items",
+            livestreams: "/api/v1/channels/f-kface/livestreams",
+            product_collections: "/api/v1/channels/f-kface/products",
+            self: "/api/v1/channels/f-kface",
+            shows: "/api/v1/channels/f-kface/shows"
+        },
+        name: "F**kFace",
+        slug: "f-kface",
+        uuid: "75ba87e8-06fd-4482-bad9-52a4da2c6181"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/friends-of-rt/episodes",
+            featured_items: "/api/v1/channels/friends-of-rt/featured_items",
+            livestreams: "/api/v1/channels/friends-of-rt/livestreams",
+            product_collections: "/api/v1/channels/friends-of-rt/products",
+            self: "/api/v1/channels/friends-of-rt",
+            shows: "/api/v1/channels/friends-of-rt/shows"
+        },
+        name: "Friends of RT",
+        slug: "friends-of-rt",
+        uuid: "cb748682-3335-4faf-8fce-1273da49aa20"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/funhaus/episodes",
+            featured_items: "/api/v1/channels/funhaus/featured_items",
+            livestreams: "/api/v1/channels/funhaus/livestreams",
+            product_collections: "/api/v1/channels/funhaus/products",
+            self: "/api/v1/channels/funhaus",
+            shows: "/api/v1/channels/funhaus/shows"
+        },
+        name: "Funhaus",
+        slug: "funhaus",
+        uuid: "2dc2a30b-55b7-443c-b565-1b3be9257fc4"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/inside-gaming/episodes",
+            featured_items: "/api/v1/channels/inside-gaming/featured_items",
+            livestreams: "/api/v1/channels/inside-gaming/livestreams",
+            product_collections: "/api/v1/channels/inside-gaming/products",
+            self: "/api/v1/channels/inside-gaming",
+            shows: "/api/v1/channels/inside-gaming/shows"
+        },
+        name: "Inside Gaming",
+        slug: "inside-gaming",
+        uuid: "8d8e8f0c-f58c-444e-bac4-1c33339bf105"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/kinda-funny/episodes",
+            featured_items: "/api/v1/channels/kinda-funny/featured_items",
+            livestreams: "/api/v1/channels/kinda-funny/livestreams",
+            product_collections: "/api/v1/channels/kinda-funny/products",
+            self: "/api/v1/channels/kinda-funny",
+            shows: "/api/v1/channels/kinda-funny/shows"
+        },
+        name: "Kinda Funny",
+        slug: "kinda-funny",
+        uuid: "6ce10ccb-f945-4b21-b157-5c5619bf1de3"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/red-vs-blue-universe/episodes",
+            featured_items: "/api/v1/channels/red-vs-blue-universe/featured_items",
+            livestreams: "/api/v1/channels/red-vs-blue-universe/livestreams",
+            product_collections: "/api/v1/channels/red-vs-blue-universe/products",
+            self: "/api/v1/channels/red-vs-blue-universe",
+            shows: "/api/v1/channels/red-vs-blue-universe/shows"
+        },
+        name: "Red vs. Blue ",
+        slug: "red-vs-blue-universe",
+        uuid: "5167de51-5719-4c1a-b9c5-ed727ef8f3a3"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/red-web/episodes",
+            featured_items: "/api/v1/channels/red-web/featured_items",
+            livestreams: "/api/v1/channels/red-web/livestreams",
+            product_collections: "/api/v1/channels/red-web/products",
+            self: "/api/v1/channels/red-web",
+            shows: "/api/v1/channels/red-web/shows"
+        },
+        name: "Red Web",
+        slug: "red-web",
+        uuid: "16d7e2f0-e67a-4bfd-9c27-427e722da888"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/rooster-teeth/episodes",
+            featured_items: "/api/v1/channels/rooster-teeth/featured_items",
+            livestreams: "/api/v1/channels/rooster-teeth/livestreams",
+            product_collections: "/api/v1/channels/rooster-teeth/products",
+            self: "/api/v1/channels/rooster-teeth",
+            shows: "/api/v1/channels/rooster-teeth/shows"
+        },
+        name: "Rooster Teeth",
+        slug: "rooster-teeth",
+        uuid: "92b6bb21-91d2-4b1b-bf95-3268fa0d9939"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/rwby-universe/episodes",
+            featured_items: "/api/v1/channels/rwby-universe/featured_items",
+            livestreams: "/api/v1/channels/rwby-universe/livestreams",
+            product_collections: "/api/v1/channels/rwby-universe/products",
+            self: "/api/v1/channels/rwby-universe",
+            shows: "/api/v1/channels/rwby-universe/shows"
+        },
+        name: "RWBY",
+        slug: "rwby-universe",
+        uuid: "92f780eb-ebfe-4bf5-a3b5-c6ad5460a5f1"
+    }, {
+        links: {
+            episodes: "/api/v1/channels/tales-from-the-stinky-dragon/episodes",
+            featured_items: "/api/v1/channels/tales-from-the-stinky-dragon/featured_items",
+            livestreams: "/api/v1/channels/tales-from-the-stinky-dragon/livestreams",
+            product_collections: "/api/v1/channels/tales-from-the-stinky-dragon/products",
+            self: "/api/v1/channels/tales-from-the-stinky-dragon",
+            shows: "/api/v1/channels/tales-from-the-stinky-dragon/shows"
+        },
+        name: "Tales From the Stinky Dragon ",
+        slug: "tales-from-the-stinky-dragon",
+        uuid: "44ef6a57-e965-40e6-942b-c28d7dec727b"
+    }]
