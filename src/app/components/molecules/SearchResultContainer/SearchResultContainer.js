59e5f23e48b00ea12ec3b3ea95146f43ecd27fa4
i3p9 <i3p9@users.noreply.github.com>
2024-03-11 15:59:14 +0600

    chore: refactor components

--- src/app/components/molecules/SearchResultContainer/SearchResultContainer.js
+++ src/app/components/molecules/SearchResultContainer/SearchResultContainer.js
@@ -1,5 +1,5 @@
 import toast, { Toaster } from 'react-hot-toast';
-import EpisodeContainer from "../fragments/EpisodeContainer/EpisodeContainer"
+import EpisodeContainer from "../../atoms/EpisodeContainer/EpisodeContainer"
 
 const SearchResultContainer = ({ data }) => {
     const notify = () => toast.success('Copied to clipboard!');
