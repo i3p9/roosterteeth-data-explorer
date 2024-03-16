c54ec9bfe4923a4564b5005fecab9edeb682415a
i3p9 <i3p9@users.noreply.github.com>
2024-03-16 14:40:32 +0600

    chore: update data url

---
+++ src/app/yikes/page.js
@@ -0,0 +1,29 @@
+async function getData() {
+    const res = await fetch(`https://archive.org/advancedsearch.php?q=uploader%3A%22sphincone%40gmail.com%22+AND+NOT+format%3A%28MP4+OR+h.264+OR+webm%29&fl%5B%5D=identifier&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=5000&page=1&output=json&callback=callback&save=yes`);
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    const data = await res.text();
+    const jsonStr = data.replace(/^callback\(|\)$/g, '');
+    const json = JSON.parse(jsonStr);
+
+    const numFound = json.response.numFound;
+
+    return numFound
+}
+
+export default async function Page() {
+    const data = await getData()
+
+    return <main>
+        <div className="container mx-auto my-5">
+            <p>total fuckup count: ~4100</p>
+            <p>remaining: {data}</p>
+            {parseInt(data) &&
+                <p>fixed: {4110 - parseInt(data)}</p>
+            }
+
+        </div>
+    </main>
+}
