668a61f2ed912203217995a75dcd683882efc760
i3p9 <i3p9@users.noreply.github.com>
2024-04-11 02:19:15 +0600

    fix: update link container on searchresult

--- src/app/components/atoms/SearchEpisodeContainer/SearchEpisodeContainer.js
+++ src/app/components/atoms/SearchEpisodeContainer/SearchEpisodeContainer.js
@@ -52,8 +52,8 @@ const SearchEpisodeContainer = ({ episode, toaster }) => {
     const thumbnailUrl = `https://cdn.ffaisal.com/thumbnail/${episode?.attributes.show_id}/${episode?.attributes.season_id ? episode?.attributes.season_id : `bonus-content-${episode?.attributes?.parent_content_slug}`}/${episode?.uuid}.jpg`
     return (
         <>
-            <Link href={`/watch/${episodeId}`}>
-                <div className="grid md:grid-cols-3 items-start max-w-6xl mx-auto px-4 gap-2 lg:gap-4 m-4 ">
+            <div className="grid md:grid-cols-3 items-start max-w-6xl mx-auto px-4 gap-2 lg:gap-4 m-4 ">
+                <Link href={`/watch/${episodeId}`}>
                     <div className="flex items-start gap-4 col-span-1">
                         <Image
                             alt="Episode thumbnail"
@@ -63,87 +63,91 @@ const SearchEpisodeContainer = ({ episode, toaster }) => {
                             width={200}
                         />
                     </div>
-                    <div className="grid gap-2 col-span-2">
+                </Link>
+
+                <div className="grid gap-2 col-span-2">
+                    <Link href={`/watch/${episodeId}`}>
                         <h3 className="font-bold text-xl sm:text-xl leading-none" style={{ display: 'flex', alignItems: 'center' }}>
                             {episode?.attributes?.title}
                         </h3>
-                        <p className="text-sm font-medium leading-none text-zinc-600">Show: {showInfo ? showInfo[0]?.attributes?.title : 'Show Data not available'} • Channel: {channelInfo[0]?.name}</p>
-                        <p className="text-xs font-medium leading-none text-zinc-600">{formatSecondToRunTime(episode?.attributes?.length)} • {episode?.attributes.original_air_date.split('T')[0]}</p>
+                    </Link>
+
+                    <p className="text-sm font-medium leading-none text-zinc-600">Show: {showInfo ? showInfo[0]?.attributes?.title : 'Show Data not available'} • Channel: {channelInfo[0]?.name}</p>
+                    <p className="text-xs font-medium leading-none text-zinc-600">{formatSecondToRunTime(episode?.attributes?.length)} • {episode?.attributes.original_air_date.split('T')[0]}</p>
+                    <Popup
+                        trigger={<p className="text-sm leading-snug line-clamp-1 text-zinc-600">{episode?.attributes?.description}</p>}
+                        modal
+                    >
+                        {(close) => (
+                            <div className='p-4'>
+                                <p data-tooltip-target="tooltip-light" data-tooltip-style="light">{episode?.attributes.description}</p>
+                                <CopyToClipboard text={episode?.attributes.description}>
+                                    <button
+                                        onClick={() => {
+                                            toaster()
+                                            close()
+                                        }}
+                                        className='w-full p-1 mt-2 border border-2 border-zinc-900 hover:bg-zinc-200'
+                                    >
+                                        Copy to clipboard
+                                    </button>
+                                </CopyToClipboard>
+                            </div>
+                        )}
+                    </Popup>
+                    <div id="tooltip-light" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 tooltip">
+                        Tooltip content
+                        <div className="tooltip-arrow" data-popper-arrow></div>
+                    </div>
+
+                    <div className='flex gap-8'>
+                        <p className='text-xs font-medium text-zinc-600'>RoosterTeeth Link: {' '}
+                            <Link className="text-xs font-medium text-blue-500" target='_blank' href={`https://roosterteeth.com/watch/${episode?.attributes.slug}`}>
+                                Click here <GoLinkExternal style={{ display: 'inline' }} />
+                            </Link>
+                        </p>
+                        <p className='text-xs font-medium text-zinc-600'>Archive Link: {' '}
+                            <Link className="text-xs font-medium text-blue-500" target='_blank' href={`https://archive.org/details/roosterteetharchive`}>
+                                <span className='text-blue-200'>N/A</span>
+                                {/* Click here <GoLinkExternal style={{ display: 'inline' }} /> */}
+                            </Link>
+                        </p>
                         <Popup
-                            trigger={<p className="text-sm leading-snug line-clamp-1 text-zinc-600">{episode?.attributes?.description}</p>}
+                            trigger={<p className='text-xs font-medium text-zinc-600'>Extended Metadata: {' '}
+                                <button className="text-xs font-medium text-blue-500">
+                                    Click here <GoLinkExternal style={{ display: 'inline' }} />
+                                </button>
+                            </p>
+                            }
                             modal
                         >
                             {(close) => (
-                                <div className='p-4'>
-                                    <p data-tooltip-target="tooltip-light" data-tooltip-style="light">{episode?.attributes.description}</p>
-                                    <CopyToClipboard text={episode?.attributes.description}>
+                                <div>
+                                    <CopyToClipboard text={JSON.stringify(episode, null, 2)}>
                                         <button
                                             onClick={() => {
                                                 toaster()
                                                 close()
                                             }}
-                                            className='w-full p-1 mt-2 border border-2 border-zinc-900 hover:bg-zinc-200'
+                                            className='static w-full p-1 mt-2 border border-2 border-zinc-900 hover:bg-zinc-200'
                                         >
                                             Copy to clipboard
                                         </button>
                                     </CopyToClipboard>
+                                    <div className='p-4' style={{ maxHeight: '80vh', overflowY: 'auto' }}>
+                                        <Log value={episode} />
+                                    </div>
+
                                 </div>
                             )}
                         </Popup>
-                        <div id="tooltip-light" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 tooltip">
-                            Tooltip content
-                            <div className="tooltip-arrow" data-popper-arrow></div>
-                        </div>
-
-                        <div className='flex gap-8'>
-                            <p className='text-xs font-medium text-zinc-600'>RoosterTeeth Link: {' '}
-                                <Link className="text-xs font-medium text-blue-500" target='_blank' href={`https://roosterteeth.com/watch/${episode?.attributes.slug}`}>
-                                    Click here <GoLinkExternal style={{ display: 'inline' }} />
-                                </Link>
-                            </p>
-                            <p className='text-xs font-medium text-zinc-600'>Archive Link: {' '}
-                                <Link className="text-xs font-medium text-blue-500" target='_blank' href={`https://archive.org/details/roosterteetharchive`}>
-                                    <span className='text-blue-200'>N/A</span>
-                                    {/* Click here <GoLinkExternal style={{ display: 'inline' }} /> */}
-                                </Link>
-                            </p>
-                            <Popup
-                                trigger={<p className='text-xs font-medium text-zinc-600'>Extended Metadata: {' '}
-                                    <button className="text-xs font-medium text-blue-500">
-                                        Click here <GoLinkExternal style={{ display: 'inline' }} />
-                                    </button>
-                                </p>
-                                }
-                                modal
-                            >
-                                {(close) => (
-                                    <div>
-                                        <CopyToClipboard text={JSON.stringify(episode, null, 2)}>
-                                            <button
-                                                onClick={() => {
-                                                    toaster()
-                                                    close()
-                                                }}
-                                                className='static w-full p-1 mt-2 border border-2 border-zinc-900 hover:bg-zinc-200'
-                                            >
-                                                Copy to clipboard
-                                            </button>
-                                        </CopyToClipboard>
-                                        <div className='p-4' style={{ maxHeight: '80vh', overflowY: 'auto' }}>
-                                            <Log value={episode} />
-                                        </div>
-
-                                    </div>
-                                )}
-                            </Popup>
-                        </div>
-                        <div>
-                            {episode?.attributes.is_sponsors_only && <FirstBadge />}
-                            {episode?.type === "bonus_feature" && <BonusContentBadge />}
-                        </div>
                     </div>
-                </div >
-            </Link>
+                    <div>
+                        {episode?.attributes.is_sponsors_only && <FirstBadge />}
+                        {episode?.type === "bonus_feature" && <BonusContentBadge />}
+                    </div>
+                </div>
+            </div >
         </>
     )
 }
