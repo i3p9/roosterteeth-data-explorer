073317931ce2f15b04cf50a5f7628b783976fad7
i3p9 <i3p9@users.noreply.github.com>
2024-04-12 15:47:22 +0600

    chore: update data (april 11)

--- src/app/browse/page.js
+++ src/app/browse/page.js
@@ -122,7 +122,7 @@ const BrowseAllShows = () => {
                                 />
                                 <Link href={`/browseshow/${item?.uuid}`}>
                                     <div
-                                        class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+                                        className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
                                 </Link>
                             </div>
                         </div>
