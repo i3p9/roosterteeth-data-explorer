779361a540048c22440caaecbf513895fa0903e5
i3p9 <i3p9@users.noreply.github.com>
2024-04-16 05:12:13 +0600

    style: update data episode loading skeleton

--- src/app/components/atoms/DataEpisodeContainer/DataEpisodeContainerSkeleton.js
+++ src/app/components/atoms/DataEpisodeContainer/DataEpisodeContainerSkeleton.js
@@ -7,13 +7,13 @@ const DataEpisodeContainerSkeleton = () => {
                 </div>
                 <div className="p-1 w-7/12 md:w-10/12">
                     <div className="font-semibold text-md mb-1">
-                        <div className="w-64 h-4 bg-gray-300 rounded"></div>
+                        <div className="w-48 md:w-64 h-4 bg-gray-300 rounded"></div>
                     </div>
                     <div className="font-semibold text-md mb-2">
-                        <div className="w-60 h-4 bg-gray-300 rounded"></div>
+                        <div className="w-36 md:w-60 h-4 bg-gray-300 rounded"></div>
                     </div>
                     <div className="text-color-secondary text-sm mb-2">
-                        <div className="w-56 h-3 bg-gray-300 rounded"></div>
+                        <div className="w-52 h-3 bg-gray-300 rounded"></div>
                     </div>
                     <div className="hidden md:block text-sm text-color-faded line-clamp-1">
                         <div className="w-full h-3 bg-gray-300 rounded"></div>
