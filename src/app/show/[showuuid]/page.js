864f804b027a854517802d7668e040245c3b460a
i3p9 <i3p9@users.noreply.github.com>
2024-10-19 06:17:57 +0600

    add dynamic metadata for show page

--- src/app/show/[showuuid]/page.js
+++ src/app/show/[showuuid]/page.js
@@ -5,6 +5,23 @@ import ShowInfo from "@/app/components/molecules/ShowInfo/ShowInfo";
 import ShowPageClientSide from "../../components/molecules/ShowPageClientSide/ShowPageClientSide";
 import AnimatedContainer from "@/app/components/atoms/AnimatedContainer/AnimatedContainer";
 
+export async function generateMetadata({ params }) {
+	const showInfo = await getShowInfoFromSlug(params.showuuid);
+
+	return {
+		title: `${makeTitle(params.showuuid)} // rt-archive`,
+		description: showInfo?.attributes.summary,
+		openGraph: {
+			title: `${makeTitle(params.showuuid)} // rt-archive`,
+			description: showInfo?.attributes.summary,
+			images: `https://cdn.rtarchive.xyz/shows/${showInfo?.uuid}/title_card.jpg`,
+			url: `https://rtarchive.xyz/show/${params.showuuid}`,
+			type: "video.episode",
+			site_name: "rtarchive",
+		},
+	};
+}
+
 export default async function ShowPage({ params }) {
 	const showInfo = await getShowInfoFromSlug(params.showuuid);
 
